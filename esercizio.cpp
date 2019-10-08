5bd94b5e02a881ce988c15ed9645b9b5c642a105
Zaccaria machil <machil674@gmail.com>
2019-10-08 10:46:04 +0200

    somma

--- esercizio.cpp
+++ esercizio.cpp
@@ -7,6 +7,11 @@ int main() {
        int a;
        int b;
        int c;
-       
+       cout << "Inscerisci il primo numero " << endl;
+       cin >> a;
+         cout << "Inscerisci il secondo numero " << endl;
+          cin >> b;
+           c=a+b;
+           cout << "la somma e'' " << endl;
       
 }
