69b2267b1b673fd4f68fbf4f7f53b04e9230a6ef
Zaccaria674 <machil674@gmail.com>
2019-10-08 17:08:08 +0200

    Modifica

--- esercizio.cpp
+++ esercizio.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
        
        int a;
-       int b;
-       int c;
-       cout << "Inscerisci il primo numero " << endl;
-       cin >> a;
-         cout << "Inscerisci il secondo numero " << endl;
-          cin >> b;
-           c=a+b;
-           cout << "la somma e'' " << endl;
-      cout << "fine programma" << endl;
+        int b;
+         int c;
+           cout << "Inscerisci il primo numero " << endl;
+             cin >> a;
+              cout << "Inscerisci il secondo numero " << endl;
+                cin >> b;
+                 c=a+b;
+                  cout << "la somma e'' " << c <<endl;
+                    cout << "fine programma" << endl;
 }
